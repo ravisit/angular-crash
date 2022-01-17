6a977a2cb28e5a2a64173eed30f82998d30117a2
Ravi <ravi.1si14cs096@gmail.com>
2022-01-17 10:01:15 +0530

    forms added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ExampleComponent } from './Components/example/example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule
